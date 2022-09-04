b1d32841fb421dd4c0b98b02231bce0305056c15
dmitkate <44602492+dmitkate@users.noreply.github.com>
2022-09-04 14:08:35 +0300

    ggggggg

--- src/api.js
+++ src/api.js
@@ -1,8 +1,6 @@
-function fetchCountries(name) { 
-    return fetch('https://restcountries.com/v3.1/name/${name}')
-        .then(response => 
-            response.json(),
-        );
-}
 
-export default { fetchCountries };
+export function fetchCountries(name) {
+    return fetch(`https://restcountries.com/v3.1/name/${name}`)
+        .then(response => response.json())
+        .catch(error => console.log(error))
+}
--- src/index.js
+++ src/index.js
@@ -1,37 +1,66 @@
-import './css/styles.css';
-import API from'./api';
-import debounce from 'lodash.debounce';
+import './css/styles.css'
+import debounce from 'lodash.debounce'
+import Notiflix from 'notiflix'
+import { fetchCountries } from './api.js'
 
-const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 300
 const refs = {
-    cardsContainer: document.querySelector('.country-info'),
-    serchForm:  document.querySelector('#search-box'),
+    countryInfo: document.querySelector('.country-info'),
+    countryInput: document.querySelector('#search-box'),
+    countryList: document.querySelector('.country-list'),
+
 };
- 
-refs.serchForm.addEventListener(
-    'input',
-    debounce(onSerch, DEBOUNCE_DELAY)
-);
 
+refs.countryInput.addEventListener('input', debounce(onCountryInput, DEBOUNCE_DELAY))
 
-function onSerch(e) {
-    e.preventDefault();
-    //const search = e.target.value.trim();
-   
-    const name = e.target.value;
+function onCountryInput() {
+    const name = refs.countryInput.value.trim()
+    if (name === '') {
+        return (refs.countryList.innerHTML = ''), (refs.countryInfo.innerHTML = '')
+    }
 
-    API.fetchCountries(name)
-    .then(renderCountryCard)
-    .catch(error => console.log(error))
-       // .finally(() => form.reset())
-        ;
+    fetchCountries(name)
+        .then(countries => {
+        refs.countryList.innerHTML = ''
+        refs.countryInfo.innerHTML = ''
+            if (countries.length === 1) {
+        refs.countryList.insertAdjacentHTML('beforeend', renderCountryList(countries))
+        refs.countryInfo.insertAdjacentHTML('beforeend', renderCountryInfo(countries))
+        } else if (countries.length >= 10) {
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.')
+        } else {
+        refs.countryList.insertAdjacentHTML('beforeend', renderCountryList(countries))
+        }
+        })
+        .catch(Notiflix.Notify.failure('Oops, there is no country with that name'))
 }
 
-
-
-
-     
-function renderCountryCard(country) {
-    const markup = '<h2>${name}</h2>';
-    refs.cardsContainer.innerHTML = markup;
+function renderCountryList(countries) {
+    const markup = countries
+    .map(({ name, flags }) => {
+        return `
+            <li>
+                <img src="${flags.svg}" alt="Flag of ${name.official}" width = 30px height = 30px>
+                <h2>${name.official}</h2>
+            </li>
+            `
+        })
+        .join('')
+    return markup
 }
+
+function renderCountryInfo(countries) {
+    const markup = countries
+    .map(({ capital, population, languages }) => {
+        return `
+        <ul>
+            <li><p>Capital: ${capital}</p></li>
+            <li><p>Population: ${population}</p></li>
+            <li><p>Languages: ${Object.values(languages).join(', ')}</p></li>
+        </ul>
+        `
+    })
+    .join('')
+    return markup
+}
+
